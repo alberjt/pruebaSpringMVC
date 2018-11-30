c1e38f9633b474b237286e69901a4e20f6bcde0e
alberjt <ajimenez@qualityobjects.com>
2018-11-30 10:01:08 +0100

    quitar libreria que no se usa

--- votaciones/src/com/votaciones/controller/VotacionesController.java
+++ votaciones/src/com/votaciones/controller/VotacionesController.java
@@ -8,7 +8,6 @@ import java.sql.Statement;
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
