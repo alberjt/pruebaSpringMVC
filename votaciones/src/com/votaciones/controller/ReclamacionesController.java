58d585fbbf7bdcda7dfb5a27019dc6f3982dfca5
Rafael <rdelolmo@qualityobjects.com>
2019-01-08 16:56:52 +0100

    CMC-692 NG: Crear reclamación Subida ficheros

--- votaciones/src/com/votaciones/controller/ReclamacionesController.java
+++ votaciones/src/com/votaciones/controller/ReclamacionesController.java
@@ -1,20 +1,22 @@
 package com.votaciones.controller;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 import javax.annotation.Resource;
 
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.votaciones.model.Reclamacion;
 import com.votaciones.services.ReclamacionesService;
 
@@ -22,27 +24,33 @@ import com.votaciones.services.ReclamacionesService;
 @CrossOrigin(origins = "*", maxAge = 3600)
 public class ReclamacionesController {
 	
+	private static String PATH = "C:\\Iberdrola\\ficheros";
+	
 	@Resource
 	private ReclamacionesService reclamacionesService;
 
 	
 	@PostMapping("/saveReclamacion")
-	public String saveVotacion(@RequestParam("file") MultipartFile file, 
-			@RequestParam("reclamacion") String reclamacion){
-		return null;
-	}	
+	public ResponseEntity<String> saveVotacion(@RequestParam(value = "file", required = false) MultipartFile file, 
+			@RequestParam("reclamacion") String reclamacionParam) throws IOException{
+		ObjectMapper objectMapper = new ObjectMapper();
+		Reclamacion reclamacion = null;
+		String nombreFichero = null;
 
-	@PostMapping("/pruebaFichero")
-	public String handleFileUpload(@RequestParam("file") MultipartFile file) {
-		 
-		try {
-			Files.copy(file.getInputStream(), Paths.get("C:\\Iberdrola").resolve(file.getOriginalFilename()));
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		reclamacion = objectMapper.readValue(reclamacionParam, Reclamacion.class);
+		if (file != null) {
+			if (!Files.exists(Paths.get(PATH))) {
+				Files.createDirectory(Paths.get(PATH));
+			}
+			nombreFichero = new String (
+					file.getOriginalFilename().getBytes(StandardCharsets.ISO_8859_1), StandardCharsets.UTF_8)
+					+ "_" + RandomStringUtils.random(4, true, false)
+					+ "." + FilenameUtils.getExtension(file.getOriginalFilename());
+			Files.copy(file.getInputStream(), Paths.get(PATH).resolve(nombreFichero));
+			reclamacion.setFichero(nombreFichero);
 		}
-		 
-		return null;
+		
+		return new ResponseEntity<String>(reclamacionesService.add(reclamacion));
 	}
 
 }
